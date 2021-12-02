099781dbc6983f8a5a2d3f57be0615e47b98c5d6
Galdino <mateusgaldino1000@hotmail.com>
2021-12-02 14:27:45 -0300

    Finish home page structure and stylization

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return(
+    <header className={styles.header}>
+      <div className={styles.headerContent}>
+        <Image src="/images/logo.svg" alt="ig.news" width="110" height="30" />
+        <Link href="/">
+          <h1 className={styles.title}>spacetraveling</h1>
+        </Link>
+        <h1 className={styles.dot}>.</h1>
+      </div>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
 
+import { BsCalendarDate, BsPerson } from 'react-icons/bs';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+
 
 interface Post {
   uid?: string;
@@ -24,9 +27,47 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  // TODO
+  return (
+    <>
+      <Head>
+        <title>spacetravelling.</title>
+      </Head>
+
+      <main className={styles.homeMainContainer}>
+        <ul className={styles.postPreviewList}>
+          <li className={styles.postPreviewContainer}>
+            <div className={styles.postPreviewContent}>
+              <h2 className={styles.postPreviewTitle}>Como utilizar hooks</h2>
+              <p className={styles.postPreviewText}>
+                Pensando em sincronização em vez de ciclos de vida.
+              </p>
+              <div className={styles.postPreviewInfo}>
+                <div className={styles.postPreviewDate}>
+                  <BsCalendarDate />
+                  <time>15 mar 2021</time>
+                </div>
+                <div className={styles.postPreviewAuthorInfo}>
+                  <BsPerson />
+                  <p>Joseph Oliveira</p>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+
+        <div className={styles.loadPostsContainer}>
+          <div className={styles.loadPostsButtonContainer}>
+            <button className={styles.loadPostsButton} type="button">
+              Carregar mais posts
+            </button>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
